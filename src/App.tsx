a04e7186e51df484a38fd78cbf7888f8c0d71ad7
Lucas Marques <lucsilva@pagar.me>
2023-02-04 16:35:02 -0300

    add font, theme and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-function App() {  
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+
+import { defaultTheme } from './styles/themes/default'
+
+function App() {
   return (
-    <div>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Hello Vite</h1>
-    </div>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
