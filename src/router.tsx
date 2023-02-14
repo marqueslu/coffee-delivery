ed1ce760a1a2bc3be86a728433b182176f491548
Lucas Marques <lucsilva@pagar.me>
2023-02-14 15:28:09 -0300

    add router for checkout page

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import { Home } from './Pages/Home'
 import { DefaultLayout } from './layouts/DefaultLayout'
+import { Checkout } from './Pages/Checkout'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Route>
     </Routes>
   )
