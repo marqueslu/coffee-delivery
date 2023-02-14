e8bb59680e39daf5cd5737b721cc1988cee48e24
Lucas Marques <lucsilva@pagar.me>
2023-02-14 15:25:19 -0300

    add button type in the actions

--- src/components/Actions/index.tsx
+++ src/components/Actions/index.tsx
@@ -11,13 +11,13 @@ interface ActionsProps {
 export function Actions({ amount, increase, decrease }: ActionsProps) {
   return (
     <S.ActionsWrapper>
-      <S.Button onClick={decrease}>
+      <S.Button type="button" onClick={decrease}>
         <Minus size={14} />
       </S.Button>
 
       <span>{amount}</span>
 
-      <S.Button onClick={increase}>
+      <S.Button type="button" onClick={increase}>
         <Plus size={14} />
       </S.Button>
     </S.ActionsWrapper>
