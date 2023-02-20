a1a2bc33cb9d976d6e6298b84b1afd5e2242f616
Lucas Marques <lucsilva@pagar.me>
2023-02-20 20:45:25 -0300

    removing duplicated code

--- src/Pages/Checkout/components/PaymentFormSection/index.tsx
+++ src/Pages/Checkout/components/PaymentFormSection/index.tsx
@@ -1,13 +1,42 @@
-import { CreditCard, CurrencyDollar } from 'phosphor-react'
+import { Bank, CreditCard, CurrencyDollar, Money } from 'phosphor-react'
 import { useTheme } from 'styled-components'
 import * as RadioGroup from '@radix-ui/react-radio-group'
 
 import { CardTitle } from '../CardTitle'
 
 import * as S from './styles'
+import { ReactNode } from 'react'
 
-export function PaymentFormSection() {
+type PaymentMethod = {
+  label: string
+  icon: ReactNode
+}
+
+interface PaymentFormSectionProps {
+  addPaymentMethod: (method: string) => void
+}
+export function PaymentFormSection({
+  addPaymentMethod,
+}: PaymentFormSectionProps) {
   const colors = useTheme()
+  const paymentMethods: PaymentMethod[] = [
+    {
+      label: 'Cartão de Crédito',
+      icon: <CreditCard size={22} color={colors['purple-500']} />,
+    },
+    {
+      label: 'Cartão de Débito',
+      icon: <Bank size={22} color={colors['purple-500']} />,
+    },
+    {
+      label: 'Dinheiro',
+      icon: <Money size={22} color={colors['purple-500']} />,
+    },
+  ]
+
+  function setPaymentMethod(method: string) {
+    addPaymentMethod(method)
+  }
 
   return (
     <S.PaymentContainer>
@@ -16,23 +45,17 @@ export function PaymentFormSection() {
         subtitle="O pagamento é feito na entrega. Escolha a forma que deseja pagar"
         icon={<CurrencyDollar size={22} color={colors['purple-500']} />}
       />
-
       <S.PaymentMethodsWrapper>
-        <RadioGroup.Root defaultValue="Dinheiro">
-          <S.PaymentOptionButton value="Cartão de Crédito">
-            <CreditCard size={16} color={colors['purple-500']} />
-            <span>Cartão de Crédito</span>
-          </S.PaymentOptionButton>
-
-          <S.PaymentOptionButton value="Cartão de Débito">
-            <CreditCard size={16} color={colors['purple-500']} />
-            <span>Cartão de Débito</span>
-          </S.PaymentOptionButton>
-
-          <S.PaymentOptionButton value="Dinheiro">
-            <CreditCard size={16} color={colors['purple-500']} />
-            <span>dinheiro</span>
-          </S.PaymentOptionButton>
+        <RadioGroup.Root
+          defaultValue="money"
+          onValueChange={(method: string) => setPaymentMethod(method)}
+        >
+          {paymentMethods.map((payment) => (
+            <S.PaymentOptionButton key={payment.label} value={payment.label}>
+              {payment.icon}
+              <span>{payment.label}</span>
+            </S.PaymentOptionButton>
+          ))}
         </RadioGroup.Root>
       </S.PaymentMethodsWrapper>
     </S.PaymentContainer>
